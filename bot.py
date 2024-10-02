6a028f43cfdaad0e3bfeecfb26d9ac361e337fa2
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2024-10-02 13:36:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,12 @@ import asyncio
 from os import environ
 import tgcrypto
 from pyrogram import Client, filters
-
+from aiohttp import web as webserver
 # Define your API ID, API Hash, and Bot Token
 API_ID = "18979569"
 API_HASH = "45db354387b8122bdf6c1b0beef93743"
 BOT_TOKEN = "6228122908:AAEKGwokHIjvYsH6qgthcz5G-sOdL3Aq45o"
+PORT_CODE = environ.get("PORT", "8080")
 
 
 # Create a new Client instance
@@ -28,7 +29,11 @@ class Bot(Client):
         await super().start()
         print("BotStarted...")
         
-
+        client = webserver.AppRunner(await bot_run())
+        await client.setup()
+        bind_address = "0.0.0.0"
+        await webserver.TCPSite(client, bind_address,
+        PORT_CODE).start()
 
 
 @bot.on_message(filters.command("start"))
